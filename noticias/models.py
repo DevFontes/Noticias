4bc365cfe7ac5b9cd52d9ca1d8e88388750bd6c7
DevFontes <41701937+DevFontes@users.noreply.github.com>
2020-03-04 23:46:15 -0300

    Update models.py

--- noticias/models.py
+++ noticias/models.py
@@ -5,11 +5,11 @@ class Noticia(Document):
     #id_noticia = 
     titulo_noticia = fields.StringField(required=True)
     texto_noticia = fields.StringField(required=True)
-    data_noticia = fields.DateField(required=False)
+    data_noticia = fields.DateField(required=False, null=True)
     autor_noticia = fields.StringField(required=True)
     
 class Autor(Document):
     #id_autor
     nome_autor = fields.StringField(required=True)
     email_autor = fields.StringField(required=True)
-    data_cadastro_autor = fields.DateField(required=True)
+    data_cadastro_autor = fields.DateField(required=False, null=True)
