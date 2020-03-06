532b1d37169f292ac9b52a48c67dcec0687a0876
DevFontes <41701937+DevFontes@users.noreply.github.com>
2020-03-06 18:42:48 -0300

    Update urls.py

--- noticias/urls.py
+++ noticias/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.NoticiaView.as_view()),
+    path("<str:pk>/", views.NoticiaView.as_view()),
 ]
