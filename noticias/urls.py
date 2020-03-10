2ac3414d7a52deff49b13457000048694b634524
DevFontes <julioesf@hotmail.com>
2020-03-09 21:44:21 -0300

    feat: adiciona bla bla bla

--- noticias/urls.py
+++ noticias/urls.py
@@ -9,3 +9,16 @@ urlpatterns = [
     #url(r'autores/', views.AutorView.as_view()),
     path("autores/", views.AutorView.as_view()),
 ]
+
+
+# boas praticas de commit
+
+# commits tem um TIPO e a DESCRIÃO, tbm podem ter outras coisas
+
+# basicamente: (À GROSSO MODO)
+
+    # feat: mensagem -> para adições de código e funcionalidades. (feat means feature)
+    # fix: mensagem -> correção de bugs
+    # chore: mensagem -> atividades que não alteram código fonte da aplicação.
+
+    # ... etc
